f0755170348617d1d174ea0c02ab836aa2b30f97
Adjéri SABI <kod047@gmail.com>
2020-10-30 15:04:18 +0900

    dummy data

--- database/seeders/HomePageSeeder.php
+++ database/seeders/HomePageSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\HomePage;
 use Illuminate\Database\Seeder;
 
 class HomePageSeeder extends Seeder
@@ -13,6 +14,11 @@ class HomePageSeeder extends Seeder
      */
     public function run()
     {
-        //
+        HomePage::create([
+            'content' => '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>',
+            'meta_title' => 'home page meta',
+            'meta_description'=> 'meta description',
+            'image' => 'Images/cda-interview-guide.jpg',
+        ]);
     }
 }
